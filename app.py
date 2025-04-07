b8b2443d2d424051da4e731df6cf077057145b85
Carlos Eduardo <eduardo201184@hotmail.com>
2025-04-07 10:45:41 -0300

    criando a janela de cadastro de clientes e serviços

--- app.py
+++ app.py
@@ -1,15 +1,42 @@
 import dearpygui.dearpygui as dpg
 
 dpg.create_context()
-dpg.create_viewport(title='janela', width=1200, height=800)
+dpg.create_viewport(title='janela', width=1000, height=500)
 
-with dpg.window(label="Emissão de Orçamentos", ):
+
+def enviar_dados_cliente():
+    nome = dpg.get_value("campo_nome")
+    email = dpg.get_value("campo_email")
+
+    print(f"Nome: {nome}")
+    print(f"email: {email}")
+
+
+def enviar_dados_servico():
+    nome_servico = dpg.get_value("campo_servico")
+    valor_servico = dpg.get_value("campo_valor")
+    print(f"Serviço: {nome_servico}")
+    print(f"Valor: {valor_servico}")
+
+
+with dpg.window(label="Emissão de Orçamentos", width=480, height=300, pos=(10, 10)):
     dpg.add_text("Cadastro de Clientes")
-    dpg.add_text("Nome:")
-    dpg.add_input_text(label="string", default_value="Nome do Cliente")
-    dpg.add_input_text(label="string", default_value="Email")
-    dpg.add_slider_float(label="float", default_value=0.273, max_value=1)
-    dpg.add_button(label="Save")
+
+    dpg.add_input_text(label="Nome:")
+
+    dpg.add_input_text(label="Email:")
+
+    dpg.add_button(label="Cadastra Cliente")
+
+with dpg.window(label="Segunda Janela", width=480, height=300, pos=(500, 10)):
+    dpg.add_text("Cadastro de Serviços")
+
+    dpg.add_input_text(label="Nome do Serviço:")
+
+    dpg.add_input_text(label="Valor:")
+
+    dpg.add_button(label="Cadastra serviço")
+
 
 dpg.setup_dearpygui()
 dpg.show_viewport()
